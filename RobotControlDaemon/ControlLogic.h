380f0a0d311710f6d8e6d864081a4aabfc991d59
Johannes Sulkakoski <johannes.sulkakoski@systemlive.fi>
2014-02-01 20:49:20 +0200

    Initial RobotControlDaemon

---
+++ RobotControlDaemon/ControlLogic.h
@@ -0,0 +1,4 @@
+
+// By: Johannes Sulkakoski, 2014
+// For: UHERobot
+// Does: Reads a named pipe
---
+++ RobotControlDaemon/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "ControlLogic.h"
+
+int main()
+{
+   std::cout << "Hello world!" << std::endl;
+}
