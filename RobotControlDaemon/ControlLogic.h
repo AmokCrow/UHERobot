8f6d0a6f9b9e0e3e92d7145ba1445aa44e7fe2f2
Johannes Sulkakoski <johannes.sulkakoski@systemlive.fi>
2014-02-04 19:24:55 +0200

    Started documentation

--- RobotControlDaemon/ControlLogic.h
+++ RobotControlDaemon/ControlLogic.h
@@ -1,4 +1,33 @@
 
 // By: Johannes Sulkakoski, 2014
 // For: UHERobot
-// Does: Reads a named pipe
+// Does: Reads a named pipe and outputs control commands
+
+#ifndef CONTROL_LOGIC__H
+#define CONTROL_LOGIC__H
+
+
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <termios.h>
+#include <unistd.h>
+
+
+
+class ControlLogic
+{
+public:
+  ControlLogic(const char const* pipeName, const char const* serialPort);
+  
+private:
+ 
+int serialFileDescriptor;
+ 
+    
+
+};
+
+#endif // CONTROL_LOGIC__H
+ 
