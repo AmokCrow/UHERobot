0c219671fd0bcb787cfe738ba3004f11ed19dee8
Johannes Sulkakoski <johannes.sulkakoski@systemlive.fi>
2014-07-28 20:40:32 +0300

    Added new board code

---
+++ RobotControlBoard2/RobotControlBoard2/src/AdcHandler.cpp
@@ -0,0 +1,19 @@
+/* 
+* AdcHandler.cpp
+*
+* Created: 27.7.2014 10:17:03
+* Author: crow
+*/
+
+
+#include "AdcHandler.h"
+
+// default constructor
+AdcHandler::AdcHandler()
+{
+} //AdcHandler
+
+// default destructor
+AdcHandler::~AdcHandler()
+{
+} //~AdcHandler
---
+++ RobotControlBoard2/RobotControlBoard2/src/config/conf_board.h
@@ -0,0 +1,14 @@
+/**
+ * \file
+ *
+ * \brief User board configuration template
+ *
+ */
+
+#ifndef CONF_BOARD_H
+#define CONF_BOARD_H
+
+#define LED0_GPIO	AVR32_PIN_PB12
+
+
+#endif // CONF_BOARD_H
