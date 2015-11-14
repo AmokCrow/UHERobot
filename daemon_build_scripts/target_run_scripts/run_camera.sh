3fce4bb2261b32affa360014184918a628112914
Johannes Sulkakoski <johannes.sulkakoski@systemlive.fi>
2015-11-14 18:02:37 +0200

    Added mjpeg-streamer to the run_camera script.

--- daemon_build_scripts/target_run_scripts/run_camera.sh
+++ daemon_build_scripts/target_run_scripts/run_camera.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
-cvlc v4l2:///dev/video0 --v4l2-width 800 --v4l2-height 600 --v4l2-chroma h264 --sout '#standard{access=http,mux=ts,dst=0.0.0.0:8554}' :demux=264 &
+# -- VLC --
+#cvlc v4l2:///dev/video0 --v4l2-width 800 --v4l2-height 600 --v4l2-chroma h264 --sout '#standard{access=http,mux=ts,dst=0.0.0.0:8554}' :demux=264 &
 #cvlc v4l2:///dev/video0 --live-caching=10 --no-audio --v4l2-width 800 --v4l2-height 600 --v4l2-chroma h264 --sout '#standard{access=http,mux=ts,dst=0.0.0.0:8554}' :demux=264
 #--network-caching 100
 #--sout-mux-caching 100
 #--http-caching=0
 #--no-audio
+
+# -- MJPG-Streamer
+
+
+cd /home/pi/mjpg-streamer/mjpg-streamer-experimental
+#export LD_LIBRARY_PATH=/home/pi/mjpg-streamer/mjpg-streamer-experimental/plugins
+./mjpg_streamer -i "./input_uvc.so -f 5 -r 320x240 -n" -o "./output_http.so -w ./www"
 
 
